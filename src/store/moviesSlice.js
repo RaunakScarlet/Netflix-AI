8ab0699e7715284bc459706b4fa4c26a2fe3b96a
Raunak Scarlet <raunakscarlet@gmail.com>
2023-10-28 14:15:32 +0530

    setUp moviesSlice and made useNowPlayingMovies custom hooks

---
+++ src/store/moviesSlice.js
@@ -0,0 +1,16 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const moviesSlice = createSlice({
+    name: 'movies',
+    initialState: {
+        nowPlayingMovies:null
+    },
+    reducers: {
+        addNowPlayingMovies: (state, action) => {
+            state.nowPlayingMovies = action.payload;
+        }
+    }
+})
+
+export const { addNowPlayingMovies } = moviesSlice.actions;
+export default moviesSlice.reducer;
